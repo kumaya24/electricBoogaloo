6318aca5be9bb7542097b46998077a6a761272c9
kumaya24 <jacquiwang05xx@gmail.com>
2024-10-19 17:18:19 -0400

    Update: program feature

--- web/flaskr/inputpage.py
+++ web/flaskr/inputpage.py
@@ -3,24 +3,30 @@ from flask import (
     Blueprint, flash, redirect, render_template, request, url_for
 )
 from flaskr.db import get_db
+from flaskr.program import large_program
 
-bp = Blueprint('inputpage', __name__)
+bp = Blueprint('inputpage', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET', 'POST'])
 def home():
+    result = None
     db = get_db()
 
     if request.method == 'POST':
-        body = request.form.get('body', '')  # Safely get form data
-        error = None
+        user_input = request.form.get('body', '')  # Safely get form data
 
-        if not body:
-            error = 'Entry is required.'
+        if not user_input:
+            flash(f"An error occurred: {str(e)}")
         else:
             try:
-                db.execute('INSERT INTO post (body) VALUES (?)', (body,))
+                # Store entry into db
+                db.execute('INSERT INTO post (body) VALUES (?)', (user_input,))
                 db.commit()
                 flash('Message saved successfully!')
+
+                # Try to run the model
+                result = large_program(user_input)
+                flash('Results is created.')
             except Exception as e:
                 db.rollback()  # Ensure consistency in case of error
                 flash(f"An error occurred: {str(e)}")
@@ -29,6 +35,4 @@ def home():
     posts = db.execute('SELECT body FROM post').fetchall()
 
     # Render the template with the retrieved posts
-    return render_template('base.html', posts=posts)
-
-
+    return render_template('base.html', posts=posts, result=result)
