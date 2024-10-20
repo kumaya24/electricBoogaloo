fbe30f4e54f35115215ac83d8bad5b6f4207ca79
kumaya24 <jacquiwang05xx@gmail.com>
2024-10-19 23:20:42 -0400

    remove some files

--- langchain_example.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("1231czx/llama3_it_ultra_list_and_bold500")
-model = AutoModelForSequenceClassification.from_pretrained("1231czx/llama3_it_ultra_list_and_bold500")
